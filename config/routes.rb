1904542dcdc8a76bc7571bbc680828aa6b468317
Kevin Wang <born2run5000@yahoo.com>
2013-06-06 18:00:43 -0400

    Add controller and views for Vote.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Stuyvote::Application.routes.draw do
+  post 'votes' => 'votes#create'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ Stuyvote::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'votes#new'
 
   # See how all your routes lay out with "rake routes"
 
