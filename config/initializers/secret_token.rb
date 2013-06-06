4b0686ce536fcaf7610c9f114b4f5f05376ea876
Kevin Wang <born2run5000@yahoo.com>
2013-06-06 16:54:46 -0400

    Add empty Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stuyvote::Application.config.secret_token = '5d341e7367a069bb48724ccd5d8d7a6a440eb9278cdb137064bf69a81056b31e2e6c7e452f5f00fcaec7b546f8d2cd8db8b6d51ba9fd1ca941c2cf7fd18ab93b'
